d800a58348eb5cbc355bcdfc86fa52337c1c57a6
TranThiNha <tranthinha160296@gmail.com>
2016-11-01 13:12:06 +0700

    Update TimelineActivity.java

--- app/src/main/java/com/codepath/apps/mysimpletweets/activity/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/activity/TimelineActivity.java
@@ -1,20 +1,25 @@
 package com.codepath.apps.mysimpletweets.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ClipDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
-import com.codepath.apps.mysimpletweets.DetailTweetActivity;
 import com.codepath.apps.mysimpletweets.PostNewTweetDialog;
+import com.codepath.apps.mysimpletweets.ReplyDialog;
 import com.codepath.apps.mysimpletweets.models.EndlessRecyclerViewScrollListener;
 import com.codepath.apps.mysimpletweets.R;
 import com.codepath.apps.mysimpletweets.adapter.TweetsArrayAdapter;
@@ -26,10 +31,8 @@ import com.google.gson.reflect.TypeToken;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import cz.msebera.android.httpclient.Header;
@@ -40,28 +43,35 @@ public class TimelineActivity extends AppCompatActivity {
     private RecyclerView rvTweets;
     private ProgressBar pbMoreLoading;
     private PostNewTweetDialog mPostNewTweetDialog;
+    private ReplyDialog mReplyDialog;
     public static String KEY_TWEET ="tweet";
     private LinearLayoutManager mLayoutManager;
+    private SwipeRefreshLayout mSwipeContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
         rvTweets = (RecyclerView) findViewById(R.id.rvResult);
+        mSwipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
         //create the arraylist
-
         //construct the adapter fromdata source
         mTweetsArrayAdapter = new TweetsArrayAdapter();
         //connect adapter to listview
+        client = TwitterApplication.getRestClient();
+        mSwipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                populateTimeline(1);
+            }
+        });
 
         mLayoutManager = new LinearLayoutManager(this);
         rvTweets.setLayoutManager(mLayoutManager);
         rvTweets.setAdapter(mTweetsArrayAdapter);
 
+
         pbMoreLoading = (ProgressBar) findViewById(R.id.pdMoreLoading);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setLogo(R.drawable.ic_twitter);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
 
         rvTweets.addOnScrollListener(new EndlessRecyclerViewScrollListener(mLayoutManager) {
             @Override
@@ -74,13 +84,25 @@ public class TimelineActivity extends AppCompatActivity {
 
         mTweetsArrayAdapter.setListClickListener(new TweetsArrayAdapter.ListClickListener() {
             @Override
-            public void onArticleItemClick(Tweet tweet)
+            public void onItemClick(Tweet tweet)
             {
                 Intent intent = new Intent(getApplicationContext(), DetailTweetActivity.class);
-                Bundle bundle = new Bundle();
-                bundle.putSerializable(KEY_TWEET,tweet);
-                intent.putExtras(bundle);
+
+                intent.putExtra("profileImage",tweet.getUser().getProfileImageUrl());
+                intent.putExtra("userName",tweet.getUser().getScreenName());
+                intent.putExtra("timeStamp",tweet.getRelativeTimestamp());
+                intent.putExtra("body",tweet.getBody());
+                intent.putExtra("retweet",tweet.getRetweetCount());
+                intent.putExtra("like",tweet.getUser().getFavouritesCount());
                 startActivity(intent);
+            }
+        });
+
+        mTweetsArrayAdapter.setReplyListener(new TweetsArrayAdapter.ReplyListener() {
+            @Override
+            public void onReplyListener(Tweet tweet) {
+               ReplyDialog replyDialog = new ReplyDialog(tweet);
+                replyDialog.shoDialog(TimelineActivity.this);
             }
         });
 
@@ -89,10 +111,7 @@ public class TimelineActivity extends AppCompatActivity {
         client = TwitterApplication.getRestClient();
         populateTimeline(1);
 
-
     }
-
-
 
     private void populateTimeline(int page) {
         client.getHomeTimeline(page,new JsonHttpResponseHandler(){
@@ -144,13 +163,16 @@ public class TimelineActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
+        final MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.compose_menu, menu);
         MenuItem postItem = menu.findItem(R.id.miEdit);
+        MenuItem profileItem = menu.findItem(R.id.miProfile);
+        MenuItem homeItem = menu.findItem(R.id.mihome);
         postItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
-                mPostNewTweetDialog = PostNewTweetDialog.newInstance("");
+                mPostNewTweetDialog = PostNewTweetDialog.newInstance();
+               /// mPostNewTweetDialog.setStyle(DialogFragment.STYLE_NORMAL,R.style.Dialog_FullScreen);
                 mPostNewTweetDialog.show(getFragmentManager(),"");
                 mPostNewTweetDialog.setOnButtonClickListener(new PostNewTweetDialog.DialogListener() {
                     @Override
@@ -163,6 +185,22 @@ public class TimelineActivity extends AppCompatActivity {
             }
         });
 
+        homeItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                Intent intent = new Intent(getApplicationContext(),TimelineActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+                return true;
+            }
+        });
+
+        profileItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
         return super.onCreateOptionsMenu(menu);
 
     }
